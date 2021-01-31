504fd4184bc93dcba63240571cbabb1cc113cc8a
Rían Errity <rian@paradaux.io>
2021-01-31 02:28:28 +0000

    Larry stubs

---
+++ src/main/java/com/larryinternational/larry/Larry.java
@@ -0,0 +1,9 @@
+package com.larryinternational.larry;
+
+public class Larry {
+
+    public static void main(String[] args) {
+        // TODO stub
+    }
+
+}
---
+++ src/test/java/com/larryinternational/larry/LarryTest.java
@@ -0,0 +1,11 @@
+package com.larryinternational.larry;
+
+import org.junit.jupiter.api.BeforeAll;
+
+class LarryTest {
+
+    @BeforeAll
+    public static void setup() {
+        // TODO stub
+    }
+}
